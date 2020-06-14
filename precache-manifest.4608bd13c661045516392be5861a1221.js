6b25ebe61c3f2102d40fe75e20f74499b145858f
rishabh <rishabh.singh@delta.in>
2020-06-14 16:33:17 +0530

    Updates

---
+++ precache-manifest.4608bd13c661045516392be5861a1221.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6b7e985ad1917b0b63c8ad9bebbab2cb",
+    "url": "/react_Employee/index.html"
+  },
+  {
+    "revision": "a0c729b73facb68ef06f",
+    "url": "/react_Employee/static/css/2.829c9cb5.chunk.css"
+  },
+  {
+    "revision": "012c53479caf0e002a73",
+    "url": "/react_Employee/static/css/main.4ccb8304.chunk.css"
+  },
+  {
+    "revision": "a0c729b73facb68ef06f",
+    "url": "/react_Employee/static/js/2.28a9a8fc.chunk.js"
+  },
+  {
+    "revision": "4ba1a1c11c57ed15aa68008707a3d6b4",
+    "url": "/react_Employee/static/js/2.28a9a8fc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "012c53479caf0e002a73",
+    "url": "/react_Employee/static/js/main.b4cbcc99.chunk.js"
+  },
+  {
+    "revision": "3dc8b237155c55df9db1",
+    "url": "/react_Employee/static/js/runtime-main.678fad05.js"
+  }
+]);
